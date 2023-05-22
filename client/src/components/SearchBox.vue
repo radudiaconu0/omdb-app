4285fb2838b84abad80cc2df2ee63920e5eb5b55
Diaconu Radu-Mihai <52667211+radudiaconu0@users.noreply.github.com>
2023-05-22 19:18:25 +0300

    Update SearchBox.vue

--- client/src/components/SearchBox.vue
+++ client/src/components/SearchBox.vue
@@ -1,13 +1,15 @@
 <script setup lang="ts">
-import { reactive, watch } from 'vue';
+import { reactive, ref, watch, nextTick } from 'vue';
 import { useMovieStore } from '../store/movie';
 import { highlightText, sanitizeHTML } from '../utils/helpers';
 import { useRouter } from "vue-router";
 import { Movie } from '../interfaces/Movie';
 import { SearchInput } from '../interfaces/SearchInput';
+import { onKeyStroke } from '@vueuse/core'
 
 const movieStore = useMovieStore();
 const router = useRouter();
+const searchBox = ref<HTMLDivElement | null>(null);
 
 const searchParams = reactive<SearchInput>({
   title: '',
@@ -58,13 +60,49 @@ const onSearchClick = async () => {
     console.error(error);
   }
 };
+
+const currentIndex = ref(-1);
+
+const resultsList = ref<HTMLUListElement>();
+
+onKeyStroke('ArrowDown', async () => {
+  if (currentIndex.value < movieStore.autocompleteResults.length - 1) {
+    currentIndex.value++;
+  }
+  console.log(currentIndex.value);
+  await nextTick();
+  resultsList.value?.children[currentIndex.value]?.scrollIntoView({ behavior: "smooth", block: "nearest" });
+}, { target: searchBox, dedupe: false });
+
+onKeyStroke('ArrowUp', async () => {
+  if (currentIndex.value > 0) {
+    currentIndex.value--;
+  }
+  console.log(currentIndex.value);
+  await nextTick();
+  resultsList.value?.children[currentIndex.value]?.scrollIntoView({ behavior: "smooth", block: "nearest" });
+}, { target: searchBox, dedupe: false });
+
+onKeyStroke('Enter', async () => {
+  if (currentIndex.value > -1) {
+    selectSearchItem(movieStore.autocompleteResults[currentIndex.value]);
+  }
+  console.log(currentIndex.value);
+  await nextTick();
+  resultsList.value?.children[currentIndex.value]?.scrollIntoView({ behavior: "smooth", block: "nearest" });
+}, { target: searchBox, dedupe: false });
+
+// Reset currentIndex when searchParams change
+watch(searchParams, () => {
+  currentIndex.value = -1
+});
+
 </script>
 
 <template>
-  <div class="dark:bg-gray-800 p-10 rounded-md">
+  <div class="dark:bg-gray-800 p-10 rounded-md" tabindex="0" ref="searchBox">
     <div class="mb-5 flex space-x-2 w-10 text-center">
-      <input
-        class="flex-grow justify-center dark:bg-gray-700 dark:text-white px-3 py-2 rounded-l-md transition-colors duration-200 ease-in-out hover:bg-gray-600"
+      <input class="flex-grow justify-center dark:bg-gray-700 dark:text-white px-3 py-2 rounded-l-md transition-colors duration-200 ease-in-out hover:bg-gray-600"
         type="text" v-model="searchParams.title" placeholder="Search by title" />
       <select
         class="dark:bg-gray-700 dark:text-white px-3 py-2 transition-colors duration-200 ease-in-out hover:bg-gray-600"
@@ -83,13 +121,13 @@ const onSearchClick = async () => {
         @click="onSearchClick">Search</button>
     </div>
     <aside
-      class="absolute top-20 left-0 max-h-96 w-75 z-10 flex flex-col items-start bg-gray-900 border rounded-md shadow-md mt-1"
-      role="menu"
-      v-if="movieStore.autocompleteResults?.length > 0">
-      <ul class="flex flex-col w-full overflow-y-scroll h-auto">
+      class="absolute top-20 left-0 max-h-96 w-100 z-10 flex flex-col items-start bg-gray-900 border rounded-md shadow-md mt-1"
+      role="menu" v-if="movieStore.autocompleteResults?.length > 0">
+      <ul class="flex flex-col w-full overflow-y-scroll h-auto" ref="resultsList">
         <li
           class="flex items-center dark:bg-gray-700 dark:text-white px-3 py-2 rounded-md transition-colors duration-200 ease-in-out hover:bg-gray-600"
-          v-for="(item, key) in movieStore.autocompleteResults" :key="key" @click="selectSearchItem(item)">
+          v-for="(item, key) in movieStore.autocompleteResults" :key="key" @click="selectSearchItem(item)"
+          :class="{ 'dark:bg-gray-600': currentIndex == key }">
           <img class="w-16 h-24 rounded-md object-cover mr-4" :src="item.Poster" :alt="item.Title" />
           <span v-html="highlightText(sanitizeHTML(item.Title), searchParams.title)"></span>
         </li>
