f94a7f6eb56f744b811d32807340f70fce5a78a0
Diaconu Radu-Mihai <52667211+radudiaconu0@users.noreply.github.com>
2023-05-22 19:26:13 +0300

    Added usage to README

--- client/src/components/MovieCard.vue
+++ client/src/components/MovieCard.vue
@@ -9,17 +9,17 @@ defineProps<{
 
 <template>
     <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 hover:scale-125 transition">
-        <a href="#">
+        <router-link :to="{ name: 'MovieDetail', params: { id: movie.imdbID } }">
             <img class="rounded-t-lg" :src="movie.Poster" alt="" />
-        </a>
+        </router-link>
         <div class="p-5">
-            <a href="#">
+            <router-link :to="{ name: 'MovieDetail', params: { id: movie.imdbID } }">
                 <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ movie.Title }}</h5>
-            </a>
+            </router-link>
             <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{{ movie.Year }}</p>
             <router-link :to="{ name: 'MovieDetail', params: { id: movie.imdbID } }"
                 class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                Read about this
+                View details
                 <svg aria-hidden="true" class="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20"
                     xmlns="http://www.w3.org/2000/svg">
                     <path fill-rule="evenodd"
